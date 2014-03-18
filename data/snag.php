60b77e14b726cb5449a7c93294036db4065fcaf3
KnotDvn <Depitche@syr.edu>
2014-03-18 12:59:20 -0400

    almost got data

--- data/snag.php
+++ data/snag.php
@@ -1,44 +1,32 @@
 <?php
 
-print_r(require_once 'auth.php');
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+require_once 'auth.php';
 
 
-$AWSAccessKeyId = Access_Key_ID;
-$SecretAccessKey = Access_Key_Secret;
- 
-$ItemId = "0679722769"; // ASIN
-$Timestamp = gmdate("Y-m-d\TH:i:s\Z");
-$Timestamp = str_replace(":", "%3A", $Timestamp);
-$ResponseGroup = "ItemAttributes,Offers,Images,Reviews";
-$ResponseGroup = str_replace(",", "%2C", $ResponseGroup);
- 
-$String = "AWSAccessKeyId=$AWSAccessKeyId&
-ItemId=$ItemId&
-AssociateTag=dvn&
-Operation=ItemLookup&
-ResponseGroup=$ResponseGroup&
-Service=AWSECommerceService&
-Timestamp=$Timestamp&
-Version=2009-01-06";
- 
-$String = str_replace("\n", "", $String);
- 
-$Prepend = "GET\nwebservices.amazon.com\n/onca/xml\n";
-$PrependString = $Prepend . $String;
- 
-$Signature = base64_encode(hash_hmac("sha256", $PrependString, $SecretAccessKey, True));  
-$Signature = str_replace("+", "%2B", $Signature);
-$Signature = str_replace("=", "%3D", $Signature);
- 
-$BaseUrl = "http://webservices.amazon.com/onca/xml?";
-$SignedRequest = $BaseUrl . $String . "&Signature=" . $Signature;
- 
-$XML = simplexml_load_file($SignedRequest);
- 
-echo '<a href="'.$SignedRequest.'">XML</a><p>';
-print_r ($XML);
+require_once "vendor/autoload.php";
 
 
 
- 
- 
+
+use ApaiIO\Configuration\GenericConfiguration;
+use ApaiIO\Operations\Search;
+use ApaiIO\ApaiIO;
+
+$conf = new GenericConfiguration();
+$conf
+    ->setCountry('com')
+    ->setAccessKey('YOUR ACCESS KEY')
+    ->setSecretKey('YOUR SECRET KEY')
+    ->setAssociateTag('YOUR ASSOCIATE TAG');
+
+$search = new Search();
+$search->setCategory('DVD');
+$search->setActor('Bruce Willis');
+$search->setKeywords('Die Hard');
+
+$apaiIo = new ApaiIO($conf);
+$response = $apaiIo->runOperation($search);
+
+var_dump($response);
