a2075d926cc3f9b40898bd0c882b9e25035a6e85
KnotDvn <Depitche@syr.edu>
2014-05-02 20:17:08 -0400

    adding conclusions

--- model/sections/conclusions.php
+++ model/sections/conclusions.php
@@ -1,10 +1,26 @@
 <div class="report-section">
                 <h1>Conclusions and After Thoughts</h1>
                     <section>
-                        <p>Template Content</p>
+
+                    	<h3>Why Yuppy</h3>
+                    	<p>This is a materialistic centered project. The heart of which involves the goal of selling somebody eles's products to Internet strangers. Yuppies, (Young Urban Professionals) are a cultural sterotype in which people purchase consumer items in order to assemble their social identity. They're the audience I would target were I to build yuppy.</p>
+                    	<p>I could have modeled <a href="http://www.gatesfoundation.org/What-We-Do/Global-Health/Malaria">Bill Gates efforts to erradicate Malaria</a> or some such other more noble endeavor. Instead I'm providing a model to calculate if it's worth it to hawk wares for a few extra bucks. Maybe I'm the yuppie-scum?</p>
+
+                    	<h3>What We Learned</h3>
+                    	<p>Picking the right products is likely where you will make it or break it. Suiting the products you advertise to your audience would be key. Selecting higher priced items to those who can afford it.</p>
+                    	<p>The data point magicX is the focal point of the high traffic model. It directly modifies visitors to customers, and customers are your ticket to revenue. Let's say you run the premier snowboarding community website, you get a prominent member to recommend your yuppy page for snowboard clothing, that reccommendation turns your visitors to customers, that recommendation is your magicX catalyst.</p>
+                    	<p>There are likely a myriad of interesting ways to acquire magicX. If you've thought of one, I advise you to exploit it because it is potentially a valuable trade secret.</p>
+                    	<h3>Big Ideas?</h3>
+                    	<p>Is there anything monumental here? Sure there is some solid web practices, decent programming, and some adequate data modeling and analysis; But is it conceptually interesting? <strong>meh</strong>, on to the next idea I say.</p>
+                    	<h3>Value?</h3>
+                    	<p>Yuppy-Model is an academic excercise first and foremost. I've acquired more legitimate experience with modern web functionality and potentially demonstrated some useful skills to prospective employers (if they overlook my blase attitude). It's an added bonus if this project nets me a passing grade. ;-) </p>
+                    	<p>It's amazing what a competent individual can do armed with a processor and a network connection. The data is out there, in amounts and locations of exponentially increasing magnitude. The effects of <a href="http://www.amazon.com/Big-Data-Revolution-Transform-Think/dp/0544002695">big data revolution are imminent</a>. Think what a skilled analyst could do with a couple trillion personal e-mails.</p>
+                    	<h3>Iterate</h3>
+                    	<p>If I was to start all over what would I do differently? Another instance of the question being more important than the answer. Yet I'd likely try to model my other idea. Calculating the profitability or reselling local Upstate NY farmers market goods at a ridiculous upscale to wealthier markets. I think the podunks and hill folk of this area have some high quality merchandise that they part with for $2.50 a quart when NYC, Seoul, Coopenhagen, or London would pay $25.00 a pint. These goods need a website and some marketing. Skills that Syracuse University students have in undervalued abundance.</p>
 
 
-popular products or products that "could be big sellers"
+                        
+
                         <p class="minimizer">
                             <span>MINIMIZE</span>
                         </p>
